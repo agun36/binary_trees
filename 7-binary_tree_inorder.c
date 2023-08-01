8da4ab92f2f2b7fafbe8ea4c27db06032c3f4da6
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-08-01 15:10:26 +0100

    Update 7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -6,6 +6,8 @@
   *
   * @*tree: is a pointer to the root node of the tree to traverse
   * @*func: is a pointer to a function to call for each node.
+  *
+  * Return:  a pointer to the created node
   */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
