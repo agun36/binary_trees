defa1c0892f5f60cf714136c61d48a34dcf9b71e
agun36 <aguntoroti10@gmail.com>
2023-08-01 07:17:59 -0700

    main

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,12 +15,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new_node = binary_tree_node(parent, value);
-	
+
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->right = parent->right;
-	
+
 	if (new_node->right != NULL)
 		new_node->right->parent = new_node;
 
