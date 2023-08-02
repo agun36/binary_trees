96fd6bc3fdd36788b737724f44efd0ba885b3f64
agun36 <aguntoroti10@gmail.com>
2023-08-02 01:33:29 -0700

    main

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,34 +1,60 @@
 #include "binary_trees.h"
 
+
 /**
- * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
- * @first: Pointer to the first node
- * @second: Pointer to the second node
- *
- * Return: Pointer to the lowest common ancestor node, or NULL if no common ancestor is found
- */
+  * binary_trees_ancestor - binary trees ancestor
+  * @first:  is a pointer to the first node
+  * @second: is a pointer to the second node
+  * Return: a pointer to the lowest common ancestor node of the two
+  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
-	const binary_tree_t *node1, *node2;
+	const binary_tree_t **ancestors_first, **ancestors_second;
+	size_t count_first, count_second  = 0;
+	size_t i, j;
+	binary_tree_t *common_ancestor = NULL;
 
 	if (first == NULL || second == NULL)
 		return (NULL);
 
-	/* Get the depths of both nodes */
-	node1 = first;
-	node2 = second;
-	while (node1 != NULL || node2 != NULL)
+	/* Create arrays to store ancestors of both nodes */
+	ancestors_first = malloc(sizeof(binary_tree_t *) * 100);
+	ancestors_second = malloc(sizeof(binary_tree_t *) * 100);
+
+	if (ancestors_first == NULL || ancestors_second == NULL)
+		return (NULL);
+
+	/* Traverse first node's ancestors and store them in the array */
+	while (first)
 	{
-		if (node1 == node2)
-			return ((binary_tree_t *)node1);
-
-		if (node1 != NULL)
-			node1 = node1->parent;
-
-		if (node2 != NULL)
-			node2 = node2->parent;
+		ancestors_first[count_first++] = first;
+		first = first->parent;
 	}
-	return (NULL);
+	/* Traverse second node's ancestors and store them in the array */
+	while (second)
+	{
+		ancestors_second[count_second++] = second;
+		second = second->parent;
+	}
+	/* Find the lowest common ancestor */
+	for (i = 0; i < count_first; i++)
+	{
+		for (j = 0; j < count_second; j++)
+		{
+			if (ancestors_first[i] == ancestors_second[j])
+			{
+				common_ancestor = (binary_tree_t *)
+					ancestors_first[i];
+				break;
+			}
+		}
+		if (common_ancestor)
+			break;
+	}
+	/* Free dynamically allocated memory */
+	free(ancestors_first);
+	free(ancestors_second);
+	
+	return (common_ancestor);
 }
-
