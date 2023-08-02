8babee990a903a5f61504bf9506cb8de6d8bf209
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-08-02 09:55:03 +0100

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -49,6 +49,9 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
- binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		 const binary_tree_t *second);
+size_t why_counters(const binary_tree_t *parents);
+int is_complete(const binary_tree_t *tree, int index, int num_);
+int binary_tree_is_complete(const binary_tree_t *tree);
 #endif
